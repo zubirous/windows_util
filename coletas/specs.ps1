dbb791815a74a8cf165aeb879539ae053102b48b
Emmanuel Dias <eajdias@gmail.com>
2026-02-03 11:30:32 -0300

    Change RAM requirement from 16 GB to 15 GB

--- coletas/specs.ps1
+++ coletas/specs.ps1
@@ -78,7 +78,7 @@ if ($gen -ge 10 -and $serie -ge 5) {
 if (-not $cpuOk) { $motivos += 'CPU' }
 
 # RAM: ≥ 16 GB total
-$ramOk = $ramTotal -ge 16
+$ramOk = $ramTotal -ge 15
 if (-not $ramOk) { $motivos += 'RAM' }
 
 # Disco: ≥ 40 GB livre
